1f5803e7f981394c0aca4e3da65a992c79b53c17
Tony Cheung <472448+tonycch@users.noreply.github.com>
2019-12-24 18:11:06 +0800

    test Actions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Auth0 Vulnerability!");
+            Console.WriteLine("Hello Auth0 Vulnerability with Actions!");
         }
     }
 }
