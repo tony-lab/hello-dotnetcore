3c582abd021d5200833197e6774c01c098535299
Tony Cheung <tonycch@github.com>
2020-01-21 16:13:12 +0800

    test gpg signing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // fixing bug 12345
+            // commit without GPG signing
             Console.WriteLine("Hello Auth0 Vulnerability with Actions!");
         }
     }
