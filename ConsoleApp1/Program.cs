a4aaeb09cf455e6679e9c2d0a5dab6e40a471dbf
Tony Cheung <tonycch@github.com>
2020-01-21 16:16:21 +0800

    no gpg from git tower

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             // fixing bug 12345
             // commit without GPG signing, even administrator
+            // from git tower
             Console.WriteLine("Hello Auth0 Vulnerability with Actions!");
         }
     }
