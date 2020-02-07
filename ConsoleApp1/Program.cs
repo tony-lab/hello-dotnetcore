74a5c493c384cf3c0acfd9c2026495f3d03b180b
Tony Cheung <tonycch@github.com>
2020-02-07 12:44:18 +0800

    added demo code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,8 +4,16 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public class Person
+        {
+            public string Fullname { get; set; }
+        }
+        
         static void Main(string[] args)
         {
+            Person tony = null;
+            System.Console.WriteLine(tony.ToString());
+
             // fixing bug 12345
             // commit without GPG signing, even administrator
             // from git tower
