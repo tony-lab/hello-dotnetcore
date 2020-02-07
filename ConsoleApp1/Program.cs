b648d2a76f5a9894736dc4dbf478116c445544d8
Tony Cheung <tonycch@github.com>
2020-02-07 12:27:09 +0800

    added tony null

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,15 +4,16 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public class Person
+        {
+            public string Fullname { get; set; }
+        }
+        
         static void Main(string[] args)
         {
-             public class Person
-            {
-                public string Fullname { get; set; }
-            }
             Person tony = null;
             System.Console.WriteLine(tony.ToString());
-            
+
             // fixing bug 12345
             // commit without GPG signing, even administrator
             // from git tower
