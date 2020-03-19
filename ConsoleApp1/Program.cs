4e41896d151e8c44fd8df6466406e14d940d21c1
Tony Cheung <tonycch@github.com>
2020-03-19 16:06:21 +0800

    lgtm demo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,19 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public class Person
+        {
+            public string Fullname { get; set; }
+        }
+        
         static void Main(string[] args)
         {
+            Person tony = null;
+            System.Console.WriteLine(tony.ToString());
+
             // fixing bug 12345
             // commit without GPG signing, even administrator
+            // from git tower
             Console.WriteLine("Hello Auth0 Vulnerability with Actions!");
         }
     }
