1271b5d6b28d07c9ef9035274a8f488fc53f88cf
Tony <tonycch@github.com>
2020-04-28 02:31:00 +0000

    null ptr exception

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,22 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        public class Person
+        {
+            public string Fullname { get; set; }
+        }
+        
         static void Main(string[] args)
         {
+            Person tony = null;
+            System.Console.WriteLine(tony.ToString());
+
             // fixing bug 12345
             // commit without GPG signing, even administrator
+            // from git tower
             Console.WriteLine("Hello Auth0 Vulnerability with Actions!");
         }
     }
